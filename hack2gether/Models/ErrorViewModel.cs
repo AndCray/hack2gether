ffae66058b1c664ff544fc987855ac97b517ad37
Andrew Crayton <ac0974769@otc.edu>
2026-04-23 18:33:09 -0500

    commit 1

---
+++ hack2gether/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hack2gether.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
